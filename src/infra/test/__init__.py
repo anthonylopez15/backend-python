fabc9592726e6b83f123102d7b6e461d25f633ed
Anthony Lopez <anthony.santillan@itriad.org.br>
2022-09-28 21:53:10 -0400

    feat: Implementing others controllers

--- src/infra/test/__init__.py
+++ src/infra/test/__init__.py
@@ -1,3 +1,5 @@
 from .user_repository_spy import UserRepositorySpy
 from .pet_repository_spy import PetRepositorySpy
 from .find_user_spy import FindUserSpy
+from .find_pet_spy import FindPetSpy
+from .register_pet_spy import RegisterPetSpy
